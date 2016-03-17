7263350b624c47a97e215412182dd77ef4a079cb
Hector A. Escobedo <ninjahector.escobedo@gmail.com>
2016-03-17 17:18:49 -0400

    Refactor for reliability and performance.
    Each connection now has separate threads for sending and receiving.
    Hanging threads and sockets have been eliminated by handling
    disconnections gracefully.
    
    The global syncmap MVar is now only updated when a connection is opened
    or closed. Each connection has its own MVar in the syncmap to indicate
    when it has been synchronized. Every other sendMessages thread will
    block while that MVar is empty, bringing processor usage down from the
    insane levels of the previous version where we incessantly refreshed and
    mapped over the syncmap.
    
    I now have a program I can be reasonably proud of!

--- src/Main.hs
+++ src/Main.hs
@@ -26,7 +26,9 @@ import Data.Maybe
 
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
-import System.IO (Handle, IOMode(..))
+import System.IO (Handle, IOMode(..), hClose, hIsWritable)
+
+type SyncMap = MVar (Map SockAddr (MVar ()))
 
 defaultPortNumber = 7034
 maxMessageSize = 4096 -- In bytes
@@ -45,54 +47,43 @@ main = do
   listen sock maxListenQueue
   -- Process connections until the program is terminated
   forever (runConnection sock shared synced)
-  return ()
 
-runConnection :: Socket -> MVar BS.ByteString -> MVar (Map SockAddr Bool) -> IO ThreadId
+runConnection :: Socket -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ThreadId
 runConnection sock shared synced = do
   (conn, addr) <- accept sock
   hand <- socketToHandle conn ReadWriteMode
-  -- So we can handle multiple connections at once
-  tid <- forkIO (talk hand addr shared synced)
-  return tid
+  forkIO (talk hand addr shared synced)
 
-talk :: Handle -> SockAddr -> MVar BS.ByteString -> MVar (Map SockAddr Bool) -> IO ()
+talk :: Handle -> SockAddr -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ()
 talk hand addr shared synced = do
-  updateSynced True
-  forever sync
-  where
-    sync = do
-      incoming
-      outgoing
-    incoming = do
-      lastMessage <- tryReadMVar shared
-      unless (isNothing lastMessage) $ do
-        updateSynced False
-        handle ignoreErr (BS.hPut hand (fromJust lastMessage))
-        updateSynced True
-        waitForAll
-        tryTakeMVar shared
-        return ()
-    outgoing = do
-      input <- BS.hGetNonBlocking hand maxMessageSize
-      unless (BS.null input) $ do
-        waitForAll
-        syncmap <- takeMVar synced
-        tryTakeMVar shared
-        putMVar shared input
-        putMVar synced (Map.map (const False) syncmap) -- Alert all
-        updateSynced True
-        waitForAll
-        tryTakeMVar shared
-        return ()
-    updateSynced b = do
-      syncmap <- takeMVar synced
-      putMVar synced (Map.insert addr b syncmap)
-    waitForAll = do -- Wait until all addresses are synced
-      syncmap <- readMVar synced
-      unless (and (Map.elems syncmap)) waitForAll
-    share a = unless (BS.null a) (putMVar shared a)
+  syncmap <- takeMVar synced
+  status <- newMVar ()
+  putMVar synced $ Map.insert addr status syncmap
+  forkIO (receiveMessages hand addr shared synced)
+  forkIO (sendMessages hand addr shared synced)
+  return ()
 
--- Very useful when we want to leave dangling sockets and threads, in case
--- someone reconnects right away. May result in major memory leakage.
-ignoreErr :: IOError -> IO ()
-ignoreErr _ = return ()
+receiveMessages :: Handle -> SockAddr -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ()
+receiveMessages hand addr shared synced = do
+  message <- BS.hGetSome hand maxMessageSize
+  unless (BS.null message) $ do
+    putMVar shared (addr, message)
+    receiveMessages hand addr shared synced
+  -- After EOF (connection closed)
+  syncmap <- takeMVar synced
+  putMVar synced $ Map.delete addr syncmap
+  hClose hand
+
+sendMessages :: Handle -> SockAddr -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ()
+sendMessages hand addr shared synced = do
+  (senderAddr, message) <- readMVar shared
+  syncmap <- readMVar synced
+  pipeOpen <- hIsWritable hand
+  let syncVar = Map.lookup addr syncmap in
+    when ((isJust syncVar) && pipeOpen) $ do
+      takeMVar (fromJust syncVar)
+      unless (senderAddr == addr) (BS.hPut hand message) -- Don't echo
+      putMVar (fromJust syncVar) ()
+      mapM_ readMVar syncmap -- Block until all connections are synced
+      tryTakeMVar shared
+      sendMessages hand addr shared synced
