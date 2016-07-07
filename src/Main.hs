d75f5c11b6445201b9481431856293d106774ba4
Hector A. Escobedo <ninjahector.escobedo@gmail.com>
2016-07-07 17:31:33 -0400

    Migrate to using STM instead of MVars.

--- src/Main.hs
+++ src/Main.hs
@@ -15,75 +15,53 @@ chat protocol. I recommend using netcat as a client.
 module Main where
 
 import Control.Concurrent
-import Control.Concurrent.MVar
-import Control.Exception
+-- import Control.Exception
+import Control.Concurrent.STM
 import Control.Monad
 
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import Data.Map (Map)
-import qualified Data.Map as Map
 import Data.Maybe
 
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import System.IO (Handle, IOMode(..), hClose, hIsWritable)
 
-type SyncMap = MVar (Map SockAddr (MVar ()))
-
 defaultPortNumber = 7034
 maxMessageSize = 4096 -- In bytes
 
 main :: IO ()
 main = do
-  shared <- newEmptyMVar -- Shared conversation buffer
-  synced <- newMVar Map.empty -- Map of synced addresses
-  -- Create a TCP socket
+  channel <- atomically newBroadcastTChan
   sock <- socket AF_INET Stream defaultProtocol
-  -- Make it reusable
   setSocketOption sock ReuseAddr 1
-  -- Set the port number to listen on
   bindSocket sock (SockAddrInet defaultPortNumber iNADDR_ANY)
-  -- Set maximum number of queued connections
   listen sock maxListenQueue
-  -- Process connections until the program is terminated
-  forever (runConnection sock shared synced)
+  forever (runConnection sock channel)
 
-runConnection :: Socket -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ThreadId
-runConnection sock shared synced = do
+runConnection :: Socket -> TChan (SockAddr, ByteString) -> IO ()
+runConnection sock channel = do
   (conn, addr) <- accept sock
   hand <- socketToHandle conn ReadWriteMode
-  forkIO (talk hand addr shared synced)
-
-talk :: Handle -> SockAddr -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ()
-talk hand addr shared synced = do
-  syncmap <- takeMVar synced
-  status <- newMVar ()
-  putMVar synced $ Map.insert addr status syncmap
-  forkIO (receiveMessages hand addr shared synced)
-  forkIO (sendMessages hand addr shared synced)
+  forkIO (receiveMessages hand addr channel)
+  clientChannel <- atomically (dupTChan channel)
+  forkIO (sendMessages hand addr clientChannel)
   return ()
 
-receiveMessages :: Handle -> SockAddr -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ()
-receiveMessages hand addr shared synced = do
+receiveMessages :: Handle -> SockAddr -> TChan (SockAddr, ByteString) -> IO ()
+receiveMessages hand addr channel = do
   message <- BS.hGetSome hand maxMessageSize
   unless (BS.null message) $ do
-    putMVar shared (addr, message)
-    receiveMessages hand addr shared synced
+    atomically (writeTChan channel (addr, message))
+    receiveMessages hand addr channel
   -- After EOF (connection closed)
-  syncmap <- takeMVar synced
-  putMVar synced $ Map.delete addr syncmap
   hClose hand
 
-sendMessages :: Handle -> SockAddr -> MVar (SockAddr, BS.ByteString) -> SyncMap -> IO ()
-sendMessages hand addr shared synced = do
-  (senderAddr, message) <- readMVar shared
-  syncmap <- readMVar synced
+-- TODO: Make this exception-safe
+sendMessages :: Handle -> SockAddr -> TChan (SockAddr, ByteString) -> IO ()
+sendMessages hand addr clientChannel = do
+  (senderAddr, message) <- atomically (readTChan clientChannel)
   pipeOpen <- hIsWritable hand
-  let syncVar = Map.lookup addr syncmap in
-    when ((isJust syncVar) && pipeOpen) $ do
-      takeMVar (fromJust syncVar)
-      unless (senderAddr == addr) (BS.hPut hand message) -- Don't echo
-      putMVar (fromJust syncVar) ()
-      mapM_ readMVar syncmap -- Block until all connections are synced
-      tryTakeMVar shared
-      sendMessages hand addr shared synced
+  when pipeOpen $ do
+    unless (senderAddr == addr) (BS.hPut hand message) -- Don't echo
+    sendMessages hand addr clientChannel
